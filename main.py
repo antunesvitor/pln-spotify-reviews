290193574b1a213dbf9e45b8c56597de8ccb847c
Vitor Guilherme Antunes <vguilhermeantunes@gmail.com>
2022-08-21 16:14:59 -0300

    Feita análise de sentimentos

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-print ("Hello World")
+from time import sleep
+from textblob import TextBlob
+from newspaper import Article
+from tqdm.auto import tqdm
+import nltk
+import pandas as pd
+
+
+# nltk.download('punkt')
+
+# url = 'https://wiki.archlinux.org/title/Installation_guide'
+# article = Article(url=url)
+
+# article.download()
+# article.parse()
+# article.nlp()
+
+
+# text = article.summary
+# text = 'I can\'t listen to my DOWNLOADED playlist while I\'m offline. So what\'s the point of this feature? Disappointed..'
+
+# blob = TextBlob(text)
+# sentiment = blob.sentiment.polarity
+# print('Texto: {};\nSentimento: {}'.format(text, sentiment))
+
+
+data = pd.read_csv('reviews.csv')
+reviews = pd.DataFrame(columns=["index", "Review", "Rating", "sentimento_pontuacao"])
+
+for row_index, registro in tqdm(iterable=data.iterrows(),total=data.shape[0]):
+    
+    indice = row_index
+    review_txt = registro["Review"]     # texto da avaliação
+    nota = registro["Rating"]           # de 1 a 5
+
+    blob = TextBlob(review_txt)
+    sentimento = blob.sentiment.polarity
+    review = pd.DataFrame([[indice, review_txt, nota, sentimento]], columns=["index", "Review", "Rating", "sentimento_pontuacao"])
+    reviews = pd.concat([reviews, review])
+    
+    sleep(0.001)
+
+    
